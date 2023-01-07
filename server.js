a5b1ed68b9d8a15e87306122151a1bdd784b254c
Swagat47 <swagatraj47@gmail.com>
2023-01-07 08:01:30 +0530

    name by time stamp

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get("/", (req, res) => {
 
 app.post('/merge', upload.array('pdfs', 2), async (req, res, next) => {
   console.log(req.files)
-  await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
-  res.redirect("http://localhost:3000/static/merged.pdf")
+  let d = await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
+  res.redirect(`http://localhost:3000/static/${d}.pdf`)
   // req.files is array of `photos` files
   // req.body will contain the text fields, if there were any
 })
